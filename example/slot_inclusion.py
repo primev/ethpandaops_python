7ce9b28011c6c92cd10e775fbd1c4c82841ba40c
Evan K <ekcopersonal@gmail.com>
2024-04-10 15:14:37 -0400

    feat: clean up example

--- example/slot_inclusion.py
+++ example/slot_inclusion.py
@@ -13,6 +13,18 @@ preprocessor: Preprocessor = Preprocessor(
 slot_inclusion_df: pl.DataFrame = preprocessor.slot_inclusion()
 
 
+# number of rows
 print(slot_inclusion_df.shape)
-print(slot_inclusion_df.head(5))
-print('done')
+# columns and datatypes
+print(slot_inclusion_df.dtypes)
+
+# df output sample of all data
+print(slot_inclusion_df.head(10))
+# ! - note that the head of data has some nulls because the mempool data and beacon chain data are not 100% synced up.
+# df output with selected columns
+print(slot_inclusion_df.drop_nulls().select('slot time', 'slot inclusion rate',
+      'slot inclusion rate (50 blob average)', 'slot target inclusion rate (2 slots)').head(10))
+
+# json output
+print(slot_inclusion_df.drop_nulls().select('slot time', 'slot inclusion rate',
+      'slot inclusion rate (50 blob average)', 'slot target inclusion rate (2 slots)').head(10).to_dicts())
