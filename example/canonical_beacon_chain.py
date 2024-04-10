b467a89254a4a5a9a485fd766e9c53219e8a3627
Evan K <ekcopersonal@gmail.com>
2024-04-10 14:08:43 -0400

    feat: update example

--- example/canonical_beacon_chain.py
+++ example/canonical_beacon_chain.py
@@ -1,10 +1,22 @@
 from ethpandaops_python.client import Queries
+import pandas as pd
 
-
+# instantiate Queries class, which contains all the query logic.
 query = Queries()
 
-print(
-    query.canonical_beacon_chain().shape
-)
+# returns pandas dataframe
+query_df: pd.DataFrame = query.canonical_beacon_chain(
+    all_cols='block_blobs', time=1, network='mainnet')
 
-print('done')
+# convert dataframe to dictionary
+query_json: dict[str] = query_df.to_dict(orient='records')
+
+# see results
+print('pandas dataframe:')
+print(query_df.head(5))
+
+print('json row 1:')
+print(query_json[0])
+
+print('json row 2:')
+print(query_json[1])
