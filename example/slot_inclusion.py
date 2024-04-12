19b586e54f1285245e9bd5bf8e3f11a9618e4682
Evan K <ekcopersonal@gmail.com>
2024-04-12 14:47:45 -0400

    fix: dataframe output to dictionary

--- example/slot_inclusion.py
+++ example/slot_inclusion.py
@@ -15,7 +15,7 @@ slot_inclusion_df: pl.DataFrame = preprocessor.create_slot_inclusion_df()
 # json output for time series
 print('\nslot inclusion time:')
 print(slot_inclusion_df.select('slot_time', 'slot_inclusion_rate',
-      'slot_inclusion_rate_50_blob_avg', '2_slot_target_inclusion_rate').head(10).to_dicts())
+      'slot_inclusion_rate_50_blob_avg', '2_slot_target_inclusion_rate').to_dicts()[:5])
 
 slot_count_breakdown_df: pl.DataFrame = preprocessor.create_slot_count_breakdown_df()
 
@@ -26,7 +26,7 @@ print(slot_count_breakdown_df.to_dicts())
 # priority gas bidding
 print('\npriority gas bidding:')
 slot_gas_bidding_df: pl.DataFrame = preprocessor.create_slot_gas_bidding_df()
-print(slot_gas_bidding_df.head(5))
+print(slot_gas_bidding_df.to_dicts()[:5])
 
 
 print('done')
