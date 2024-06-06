51ebc190b66b4f4803a67d27a8298aefc9fce6c6
Evan K <ekcopersonal@gmail.com>
2024-06-06 18:38:12 -0400

    feat: remove polars transformations from Preprocessor class

--- example/query_benchmark.py
+++ example/query_benchmark.py
@@ -1,13 +1,12 @@
 from ethpandaops_python.preprocessor import Preprocessor
-import polars as pl
 
 # instantiate a Preprocessor for Base blob data fora 1 day period
 preprocessor: Preprocessor = Preprocessor(
-    period=30,     # 1 day
+    period=1,     # 1 day
     network='mainnet'   # mainnet
 )
 
-# 4/24/24 Hypersync query is timing out, would need to update to parquet query config. This is a blocker for historical data access.
+# write dataframes to parquet files
 preprocessor.cached_data['mempool_df'].write_parquet(
     'mempool_df.parquet')
 preprocessor.cached_data['canonical_beacon_blob_sidecar_df'].write_parquet(
