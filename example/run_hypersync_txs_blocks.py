d96d3a95f1c6546611c2709a197fb86c5cc7d0e1
Evan K <ekcopersonal@gmail.com>
2024-06-09 13:05:32 -0400

    chore: remove old files

--- example/run_hypersync_txs_blocks.py
+++
@@ -1,9 +0,0 @@
-from ethpandaops_python.hypersync import Hypersync
-
-client = Hypersync()
-
-df = client.query_txs("0x5050f69a9786f081509234f1a7f4684b5e5b76c9", 1)
-
-
-print(df.shape)
-print(df.head(5))
